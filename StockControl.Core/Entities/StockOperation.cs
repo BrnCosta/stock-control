3bd63a9097d95dc0e7a3f4d5660626fb446af8a8
BrnCosta <brunooc.bc@gmail.com>
2024-10-30 20:46:49 -0300

    Adding tax to stock operations

--- StockControl.Core/Entities/StockOperation.cs
+++ StockControl.Core/Entities/StockOperation.cs
@@ -3,14 +3,15 @@
 namespace StockControl.Core.Entities
 {
   public class StockOperation
-    {
-        public int? Id { get; set; }
-        public required int Quantity { get; set; }
-        public required double Price { get; set; }
-        public required DateTime Date { get; set; }
-        public required OperationType OperatingType { get; set; }
+  {
+    public int? Id { get; set; }
+    public required int Quantity { get; set; }
+    public required double Price { get; set; }
+    public double? Tax { get; set; }
+    public required DateTime Date { get; set; }
+    public required OperationType OperatingType { get; set; }
 
-        // Relationships
-        public required string StockSymbol { get; set; }
-    }
+    // Relationships
+    public required string StockSymbol { get; set; }
+  }
 }
